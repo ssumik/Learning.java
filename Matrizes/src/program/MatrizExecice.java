7c2d87858b9904794c57d08caeb23d6528b3a423
ssumik <joao.sumi@gmail.com>
2024-10-04 13:48:18 -0500

    Updated code

--- Matrizes/src/program/MatrizExecice.java
+++ Matrizes/src/program/MatrizExecice.java
@@ -12,31 +12,31 @@ public class MatrizExecice {
 		sc.nextLine();
 		int[][] matriz = new int[n][n];
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				System.out.printf("numero na linha %d, coluna %d ", i, j);
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.printf("digite os tres numeros da linha %d separados por espaço: ", i);
+			for (int j = 0; j < matriz[i].length; j++) {
 				matriz[i][j] = sc.nextInt();
 			}
 		}
 
 
 		System.out.println("matriz:");
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
 				System.out.printf("%d ", matriz[i][j]);
 			}
 			System.out.println();
 		}
 
 		System.out.print("diagonal pricipal: ");
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < matriz.length; i++) {
 			
 			System.out.printf("%d ", matriz[i][i]);
 		}
 
 		int negativeNum = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
 				if (matriz[i][j] < 0) {
 					negativeNum++;
 				}
